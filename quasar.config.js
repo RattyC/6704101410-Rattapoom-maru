19d08f1089a00d753a5757341a5fd57515e43b79
rattyC <rattapoommaru@gmail.com>
2025-10-08 11:47:47 +0700

    edi q

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/id-name-lastname/',
+      publicPath: '/6704101410-Rattapoom-maru/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
